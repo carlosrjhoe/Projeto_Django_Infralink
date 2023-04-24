f6c9fbd87d62889036b4ee672bae4b7c59f88037
Carlos Roberto <carlosrjhoe@gmail.com>
2023-04-24 08:41:54 -0300

    Foi criado o models Funcionario

--- venv/funcionarios/models.py
+++ venv/funcionarios/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Funcionario(models.Model):
+
+    nome = models.CharField(max_length=255, null=False, blank=True)
+    sobreNome = models.CharField(max_length=255, null=False, blank=True)
+    cpf = models.CharField(max_length=255, null=False, blank=True)
+    tempo_de_servico = models.PositiveIntegerField(default=0, null=False, blank=True)
+    remuneracao = models.DecimalField(max_digits=8, decimal_places=2)
+
+    
+    def __str__(self):
+        return 
+
+    objetos = models.Manager()
+ 
